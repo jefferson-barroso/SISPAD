c6da97dd758d9bb8eb70334008c08bd6dbc1ac60
jefferson.freitas <jefersson.freitas@memora.com.br>
2025-05-20 11:56:24 -0300

    feat: criacao dos componentes e da tela inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+import { CadastrarUsuarioComponent } from './tela-inicial/cadastrar-usuario/cadastrar-usuario.component';
+import { ListarCadastrosComponent } from './tela-inicial/listar-cadastros/listar-cadastros.component';
+import { BuscarUsuarioComponent } from './tela-inicial/buscar-usuario/buscar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaInicialComponent,
+    CadastrarUsuarioComponent,
+    ListarCadastrosComponent,
+    BuscarUsuarioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tela-inicial/listar-cadastros/listar-cadastros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-cadastros',
+  templateUrl: './listar-cadastros.component.html',
+  styleUrls: ['./listar-cadastros.component.scss']
+})
+export class ListarCadastrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
